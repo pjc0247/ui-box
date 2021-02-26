3167f36bc70ec690acf5f87c73ce4b94f5e302ac
Park June Chul <pjc0247@naver.com>
2021-02-26 18:19:47 +0900

    Update CodeInput.js

--- animated-ui/src/component/input/CodeInput.js
+++ animated-ui/src/component/input/CodeInput.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
-import { FadeIn } from '../../css';
 
+import { FadeIn } from '../../css';
 import { Space } from '../layout';
 
 const Container = styled.div`
